97036b211af7668b787b7ddf8cc110f871f6da72
boyan zahariev <zaharievboyan@gmail.com>
2021-03-11 22:58:49 +0200

    rename frmSignup to form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.form = this.createSignupForm();
   }
 
+  ngOnInit(): void {}
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+  createSignupForm(): FormGroup {
+    return this.fb.group(
+      {
+        // email is required and must be a valid email email
+        email: [
+          null,
+          Validators.compose([Validators.email, Validators.required]),
+        ],
+        password: [
+          null,
+          Validators.compose([
+            // 1. Password Field is Required
+            Validators.required,
+            // 2. check whether the entered password has a number
+            CustomValidators.patternValidator(/\d/, { hasNumber: true }),
+            // 3. check whether the entered password has upper case letter
+            CustomValidators.patternValidator(/[A-Z]/, {
+              hasCapitalCase: true,
+            }),
+            // 4. check whether the entered password has a lower-case letter
+            CustomValidators.patternValidator(/[a-z]/, { hasSmallCase: true }),
+            // 5. check whether the entered password has a special character
+            CustomValidators.patternValidator(
+              /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+              {
+                hasSpecialCharacters: true,
+              }
+            ),
+            Validators.minLength(8),
+          ]),
+        ],
+        confirmPassword: [null, Validators.compose([Validators.required])],
+      },
+      {
+        // check whether our password and confirm password match
+        validator: CustomValidators.passwordMatchValidator,
+      }
+    );
+  }
 }
