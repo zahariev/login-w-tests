f7cdc0e40e427dbc74a3e5e6078725c5d7403461
boyan zahariev <zaharievboyan@gmail.com>
2021-03-11 23:01:53 +0200

    rename frm

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,9 +9,9 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +22,26 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render form with email and password inputs', () => {
+    const element = fixture.nativeElement;
+
+    expect(element.querySelector('form')).toBeTruthy();
+    expect(element.querySelector('#email')).toBeTruthy();
+    expect(element.querySelector('#password')).toBeTruthy();
+    expect(element.querySelector('#confirmPassword')).toBeTruthy();
+    expect(element.querySelector('button')).toBeTruthy();
+  });
+
+  it('should return model invalid when form is empty', () => {
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('should validate email input as required', () => {
+    const email = component.form.controls.email;
+
+    expect(email.valid).toBeFalsy();
+    expect(email.errors.required).toBeTruthy();
   });
 });
