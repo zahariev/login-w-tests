c92a593cda3df72873978ac00ed131b7d4e869cc
boyan zahariev <zaharievboyan@gmail.com>
2021-03-14 21:54:02 +0200

    add: register form validation

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -44,4 +44,67 @@ describe('RegisterComponent', () => {
     expect(email.valid).toBeFalsy();
     expect(email.errors.required).toBeTruthy();
   });
+
+  it('should validate password input as required', () => {
+    const password = component.form.controls.password;
+
+    expect(password.valid).toBeFalsy();
+    expect(password.errors.required).toBeTruthy();
+  });
+
+  it('should validate email format', () => {
+    const email = component.form.controls.email;
+    email.setValue('test');
+    const errors = email.errors;
+
+    expect(errors.required).toBeFalsy();
+    expect(errors.pattern).toBeTruthy();
+    expect(email.valid).toBeFalsy();
+  });
+
+  it('should validate email format correctly', () => {
+    const email = component.form.controls.email;
+    email.setValue('test@test.com');
+    const errors = email.errors || {};
+
+    expect(email.valid).toBeTruthy();
+    expect(errors.required).toBeFalsy();
+    expect(errors.pattern).toBeFalsy();
+  });
+
+  it('should validate missing number in new password', () => {
+    const password = component.form.controls.password;
+    password.setValue('Asdd!');
+    const errors = password.errors || {};
+
+    expect(errors.hasNumber).toBeDefined();
+    expect(errors.hasNumber).toBeTrue();
+  });
+
+  it('should validate missing spec char in new password', () => {
+    const password = component.form.controls.password;
+    password.setValue('Asdd1');
+    const errors = password.errors || {};
+
+    expect(errors.hasSpecialCharacters).toBeDefined();
+    expect(errors.hasSpecialCharacters).toBeTrue();
+  });
+
+  it('should validate missing CapitalCase char in new password', () => {
+    const password = component.form.controls.password;
+    password.setValue('!asdd1');
+    const errors = password.errors || {};
+
+    expect(errors.hasCapitalCase).toBeDefined();
+    expect(errors.hasCapitalCase).toBeTrue();
+  });
+
+  it('should validate missing LowerCase char in new password', () => {
+    const password = component.form.controls.password;
+    password.setValue('!AA1');
+    const errors = password.errors || {};
+
+    expect(errors.hasLowerCase).toBeDefined();
+    expect(errors.hasLowerCase).toBeTrue();
+  });
 });
