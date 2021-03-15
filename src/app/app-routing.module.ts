670fd6b185567c9741ba0c75e0d1e0148f9a7f39
boyan zahariev <zaharievboyan@gmail.com>
2021-03-16 01:22:25 +0200

    fix: wrong path 404 to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./login/login.module').then((m) => m.LoginModule),
-    data: { preload: true, breadcrumb: 'Home' },
-  },
+  // {
+  //   path: 'login',
+  //   loadChildren: () =>
+  //     import('./login/login.module').then((m) => m.LoginModule),
+  //   data: { preload: true, breadcrumb: 'Home' },
+  // },
+  // { path: '', pathMatch: 'full', redirectTo: 'home' },
+
   {
     path: 'login',
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginModule),
-    data: { preload: true, breadcrumb: 'Home' },
+    data: { preload: true, breadcrumb: 'Login' },
   },
   {
     path: 'register',
     loadChildren: () =>
       import('./register/register.module').then((m) => m.RegisterModule),
-    data: { preload: true, breadcrumb: 'Home' },
+    data: { preload: true, breadcrumb: 'Register' },
   },
+  { path: '**', redirectTo: '/login' },
 ];
 
 @NgModule({
