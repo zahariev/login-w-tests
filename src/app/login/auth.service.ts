5717a6ea17e708efc3e8ccb4e87c5e92dd40b677
boyan zahariev <zaharievboyan@gmail.com>
2021-03-16 01:22:39 +0200

    auth service methods

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+const BASEURL = 'http://localhost:3000/api/resetpassword';
 @Injectable({
   providedIn: 'root',
 })
@@ -13,5 +14,25 @@ export class AuthService {
       email,
       password,
     });
+  }
+
+  registerUser(body): Observable<any> {
+    return this.httpClient.post(`${BASEURL}/register`, body);
+  }
+
+  loginUser(body): Observable<any> {
+    return this.httpClient.post(`${BASEURL}/login`, body);
+  }
+
+  requestReset(body): Observable<any> {
+    return this.httpClient.post(`${BASEURL}/req-reset-password`, body);
+  }
+
+  newPassword(body): Observable<any> {
+    return this.httpClient.post(`${BASEURL}/new-password`, body);
+  }
+
+  validPasswordToken(body): Observable<any> {
+    return this.httpClient.post(`${BASEURL}/valid-password-token`, body);
   }
 }
