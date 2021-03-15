6db72d499eb8568d125089aba3d143cbb562d347
boyan zahariev <zaharievboyan@gmail.com>
2021-03-16 01:20:51 +0200

    add: login routing for reset password

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { ResetComponent } from './reset/reset.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
-    data: { breadcrumb: 'Login' },
+    data: { breadcrumb: '' },
+  },
+
+  {
+    path: 'reset-password',
+    component: ResetComponent,
+    data: { breadcrumb: 'Reset-password' },
   },
 ];
 
