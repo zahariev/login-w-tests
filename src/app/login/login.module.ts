add56f77008be2a81a3c3ce9b82c0dc4d45eead4
boyan zahariev <zaharievboyan@gmail.com>
2021-03-16 01:22:03 +0200

    add: forgot password ancor

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,9 +5,10 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from './auth.service';
+import { ResetComponent } from './reset/reset.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, ResetComponent],
   imports: [CommonModule, LoginRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [AuthService],
 })
