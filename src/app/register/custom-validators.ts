a0b1f9155a947b657bd1619b5ba04ac8d39c7eaa
boyan zahariev <zaharievboyan@gmail.com>
2021-03-16 01:19:19 +0200

    fix: type on error message

--- src/app/register/custom-validators.ts
+++ src/app/register/custom-validators.ts
@@ -22,7 +22,7 @@ export class CustomValidators {
     // compare is the password math
     if (password !== confirmPassword) {
       // if they don't match, set an error in our confirmPassword form control
-      control.get('confirmPassword').setErrors({ NoPassswordMatch: true });
+      control.get('confirmPassword').setErrors({ noPasswordMatch: true });
     }
   }
 }
